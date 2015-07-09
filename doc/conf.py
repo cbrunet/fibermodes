7359a303d60cc4dcc6344300b3a76a8176fd78f2
Charles Brunet <charles@cbrunet.net>
2015-07-09 13:11:46 -0400

    Updated RTD settings

--- doc/conf.py
+++ doc/conf.py
@@ -16,12 +16,15 @@
 import sys
 import os
 
+from unittest.mock import MagicMock
 
-import mock
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
 
 MOCK_MODULES = ['numpy', 'scipy']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
