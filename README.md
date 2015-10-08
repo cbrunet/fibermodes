7e7c849ff22f4ae1815f5475bc312afd67dcf09b
Charles Brunet <charles@cbrunet.net>
2015-10-08 14:50:51 -0400

    skip cuda tests.

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ Multilayers fiber mode solver
 [![Build Status](https://travis-ci.org/cbrunet/fibermodes.svg?branch=master)](https://travis-ci.org/cbrunet/fibermodes)
 
 API documentation available on http://fibermodes.rtfd.org/
+
+
+Installation
+============
+
+Requirements:
+
+- Python >= 3.4
+- numpy
+- scipy
+
+ For GUI:
+
+ - pyside
+ - pyqtgraph
--- tests/fiber/solver/cuda.py
+++ tests/fiber/solver/cuda.py
@@ -1,23 +1,29 @@
 """Test suite for fibermodes.fiber.solver.cuda"""
 
 import unittest
-from tests.fiber.solver.tlsif import TestTLSIF
-from fibermodes.fiber.solver import cuda
+
+try:
+    from tests.fiber.solver.tlsif import TestTLSIF
+    from fibermodes.fiber.solver import cuda
 
 
-@unittest.skip("Implementation not finished")
-class TestCuda(TestTLSIF):
+    @unittest.skip("Implementation not finished")
+    class TestCuda(TestTLSIF):
 
-    """Test suite for PSimulator class"""
+        """Test suite for PSimulator class"""
 
-    def setUp(self):
-        super().setUp()
-        self.f.setSolvers(neff=cuda.Neff)
+        def setUp(self):
+            super().setUp()
+            self.f.setSolvers(neff=cuda.Neff)
+
+except e:
+    class TestCuda(unittest.TestCase):
+
+        @unittest.skip('Missing dependency - ' + e.message)
+        def test_fail():
+            pass
 
 if __name__ == "__main__":
     # logging.basicConfig(level=logging.DEBUG)
-    import os
-    os.chdir("../..")
-    # unittest.main()
     suite = unittest.TestLoader().loadTestsFromTestCase(TestCuda)
     unittest.TextTestRunner(verbosity=1).run(suite)
