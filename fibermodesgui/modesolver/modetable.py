a51436cc2936ce543617ed36cd3bbc718c397890
Charles Brunet <charles@cbrunet.net>
2015-12-04 10:21:30 -0500

    Corrected display of beta in the table

--- fibermodesgui/modesolver/modetable.py
+++ fibermodesgui/modesolver/modetable.py
@@ -1,3 +1,17 @@
+# This file is part of FiberModes.
+#
+# FiberModes is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FiberModes is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FiberModes.  If not, see <http://www.gnu.org/licenses/>.
 
 from PySide import QtGui, QtCore
 from math import isnan, isinf
@@ -38,7 +52,7 @@ PARAMS = {
     "vg": (1e-6, "m / us"),
     "D": (1, "ps / (nm km)"),
     "S": (1, "ps / (nm² km)"),
-    "beta0": (1e-9, "1 / nm"),
+    "beta0": (1e-9, "nm⁻¹"),
     "beta1": (1e3, "ps / nm"),
     "beta2": (1e15, "ps² / nm"),
     "beta3": (1e27, "ps³ / nm"),
@@ -98,7 +112,7 @@ class ModeTableModel(QtCore.QAbstractTableModel):
             else:
                 if u:
                     u = " "+u
-                return "{:.5f}{}".format(v*m, u)
+                return "{:.5g}{}".format(v*m, u)
         elif role == QtCore.Qt.ToolTipRole:
             if v is None:
                 return None
